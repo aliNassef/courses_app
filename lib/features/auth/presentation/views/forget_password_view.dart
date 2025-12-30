dcc570b7b4dfb30c42576ebc61a6f782e61a3f08
aliNassef <anassef798@gmail.com>
2025-12-30 16:26:23 +0200

    Add forget password view and navigation
    Introduced ForgetPasswordView and its body widget for password reset functionality. Updated app_routes to support navigation to the new view and modified the login view to navigate to the forget password screen when 'Forgot Password?' is pressed. Also adjusted DefaultAppButton's minSize for consistent button height.

---
+++ lib/features/auth/presentation/views/forget_password_view.dart
@@ -0,0 +1,28 @@
+import 'package:adaptive_platform_ui/adaptive_platform_ui.dart';
+import 'package:courses_app/core/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/extensions/mediaquery_size.dart';
+import '../../../../core/widgets/widgets.dart';
+import '../widgets/forget_password_view_body.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+
+  static const String routeName = 'forget-password';
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveScaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          height: context.height,
+          width: context.width,
+          color: AppColors.white,
+          child: const ForgetPasswordViewBody(),
+        ),
+      ),
+    );
+  }
+}
